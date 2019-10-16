1a7d81298bc3728e146fe63cb687d66615b9ffd0
sachin156 <sachinvadlakonda999@gmail.com>
2019-10-16 12:09:10 +0530

    Mixins

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -7,55 +8,47 @@ from django.views.generic import (
     DeleteView,
     UpdateView
     )
+
 from .models import Article
 from .forms import ArticleForm
 
-# Create your views here.
 
-# deleteview, updateview,
+# Mixins..
+# to use these classes in other main class..
+class GetObjectMixin(object):
+
+    def get_object(self):
+        id_=self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+class FormValidMixin(object):
+
+    def form_valid(self,form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+# ******
 
 class ArticleListView(ListView):
     template_name='articles/article_list.html'
     queryset=Article.objects.all()
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(FormValidMixin,CreateView):
     template_name='articles/article_create.html'
     form_class=ArticleForm
     queryset=Article.objects.all()
 
-    def form_valid(self,form):
-        print(form.cleaned_data)
-        return super().form_valid(form)
 
-
-class ArticleDetailView(DetailView):
+class ArticleDetailView(GetObjectMixin,DetailView):
     template_name='articles/article_detail.html'
 
-    def get_object(self):
-        id_=self.kwargs.get("id")
-        return get_object_or_404(Article, id=id_)
-
-
-
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(GetObjectMixin,FormValidMixin,UpdateView):
     template_name='articles/article_create.html'
     form_class=ArticleForm
 
-    def get_object(self):
-        id_=self.kwargs.get("id")
-        return get_object_or_404(Article, id=id_)
-
-    def form_valid(self,form):
-        print(form.cleaned_data)
-        return super().form_valid(form) 
-
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(GetObjectMixin,DeleteView):
     template_name='articles/article_delete.html'
-    
-    def get_object(self):
-        id_=self.kwargs.get("id")
-        return get_object_or_404(Article, id=id_)
     
     def get_success_url(self):
         return reverse('articles:article-list')
